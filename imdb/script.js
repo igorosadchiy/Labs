807aa477e348a46be404ac8f7192698d26e767e1
Igor Osadchiy <egor.osadchiy@gmail.com>
2018-07-27 22:55:54 +0300

    radiobuttons ready

--- imdb/script.js
+++ imdb/script.js
@@ -2,21 +2,34 @@
 const API_KEY = 'fd58e48d';
 const url = `http://www.omdbapi.com/?apikey=${API_KEY}`;
 const form = document.forms.search_form;
+const alertMessage = form.querySelector('.alert-message');
 
 form.addEventListener('submit', (event) => {
 	"use strict";
     event.preventDefault();
-    const value = form.title.value.trim();
-    if (!value) {
+    const searchValue = form.title.value.trim();
+	const typeValue = form.typeCheck.value;
+    if (!searchValue) {
         form.title.classList.add('error');
-        setVisibility(form.querySelector('.error-message'), true);
+		alertMessage.classList.add('text-danger');
+		alertMessage.innerHTML = 'This field is required!';
+        setVisibility(alertMessage, true);
     } else {
-        fetch(`${url}&s=${value}`)
+		document.getElementById("card-container").innerHTML = '';
+        fetch(`${url}&s=${searchValue}&type=${typeValue}`)
             .then( response => response.json() )
             .then( data => generateResultCards(data) );
-        form.title.classList.remove('error');
-        setVisibility(form.querySelector('.error-message'), false);
     } 
+});
+
+form.title.addEventListener('focus', (event) => {
+	"use strict";
+    event.preventDefault();
+	if (alertMessage.classList.value.indexOf("text-danger") > 0) {
+		form.title.classList.remove('error');
+		alertMessage.classList.remove('text-danger');
+    	setVisibility(alertMessage, false);
+	}
 });
 
 function setVisibility(element, isError) {
@@ -30,22 +43,36 @@ function setVisibility(element, isError) {
 
 function generateResultCards(data) {
 	"use strict";
+	if (data.Response === "False") {
+		alertMessage.classList.remove('text-success');
+		alertMessage.classList.add('text-danger');
+		alertMessage.innerHTML = `Not Found!`;
+		setVisibility(alertMessage, true);
+		return;
+	}
 	var searchArray = data.Search;
 	var cardArray = [];
+	alertMessage.classList.remove('text-danger');
+	alertMessage.classList.add('text-success');
+	alertMessage.innerHTML = `Found ${data.totalResults} movies`;
+    setVisibility(alertMessage, true);
 	for (var i = 0; i < searchArray.length; i++) {
 		cardArray.push (`
 			<div class="card">
-				<img src="${searchArray[i].Poster}" alt="Card image cap" class="card-img-top">
+				<a href="#" class="poster-wrap" data-toggle="modal" data-id="${searchArray[i].imdbID}" data-target="#modalMoreInfo" title="Click to more info" >
+					<img src="${searchArray[i].Poster}" alt="${searchArray[i].Title}" class="card-img-top">
+				</a>
 				<div class="card-body">
 					<h5 class="card-title">${searchArray[i].Title}</h5>
 					<ul class="list-group list-group-flush">
 						<li class="list-group-item"><b>Year:</b> ${searchArray[i].Year}</li>
 						<li class="list-group-item"><b>ID:</b> ${searchArray[i].imdbID}</li>
 					</ul>
-					<a href="#" class="btn btn-primary btn-card">More info</a>
+					<button type="button" class="btn btn-primary mt-3" data-toggle="modal" data-id="${searchArray[i].imdbID}" data-target="#modalMoreInfo">More info</button>
+					<button type="button" class="btn btn-outline-warning mt-3 float-right" data-id="${searchArray[i].imdbID}"><i class="fa fa-star-o"></i></button>
 				</div>
 			</div>
 		`);
 	}
-	document.getElementById("search-result").innerHTML = cardArray.join('');
+	document.getElementById("card-container").innerHTML = cardArray.join('');
 }
