f89a9588ac1a0058e66fccba7f552b972c051a06
Igor Osadchiy <egor.osadchiy@gmail.com>
2018-07-29 14:35:26 +0300

    modal & add/remove favorites ready

--- imdb/script.js
+++ imdb/script.js
@@ -50,16 +50,23 @@ function generateResultCards(data) {
 		setVisibility(alertMessage, true);
 		return;
 	}
-	var searchArray = data.Search;
-	var cardArray = [];
+	let searchArray = data.Search;
+	let cardArray = [];
+	let localstorege = JSON.stringify(localStorage);
+	let favorite ='';
 	alertMessage.classList.remove('text-danger');
 	alertMessage.classList.add('text-success');
 	alertMessage.innerHTML = `Found ${data.totalResults} movies`;
     setVisibility(alertMessage, true);
 	for (var i = 0; i < searchArray.length; i++) {
+		if (localstorege.indexOf(searchArray[i].imdbID) > 0) {
+			favorite = 'active';
+		} else {
+			favorite = '';
+		}
 		cardArray.push (`
 			<div class="card">
-				<a href="#" class="poster-wrap" data-toggle="modal" data-id="${searchArray[i].imdbID}" data-target="#modalMoreInfo" title="Click to more info" >
+				<a href="#" class="poster-wrap" data-id="${searchArray[i].imdbID}" title="Click to more info" >
 					<img src="${searchArray[i].Poster}" alt="${searchArray[i].Title}" class="card-img-top">
 				</a>
 				<div class="card-body">
@@ -68,11 +75,101 @@ function generateResultCards(data) {
 						<li class="list-group-item"><b>Year:</b> ${searchArray[i].Year}</li>
 						<li class="list-group-item"><b>ID:</b> ${searchArray[i].imdbID}</li>
 					</ul>
-					<button type="button" class="btn btn-primary mt-3" data-toggle="modal" data-id="${searchArray[i].imdbID}" data-target="#modalMoreInfo">More info</button>
-					<button type="button" class="btn btn-outline-warning mt-3 float-right" data-id="${searchArray[i].imdbID}"><i class="fa fa-star-o"></i></button>
+					<button type="button" class="btn-more-info btn btn-primary mt-3" data-id="${searchArray[i].imdbID}">More info</button>
+					<button type="button" class="btn-favorite btn btn-outline-warning mt-3 float-right ${favorite}" id="${searchArray[i].imdbID}" data-id="${searchArray[i].imdbID}"><i class="fa fa-star-o"></i></button>
 				</div>
 			</div>
 		`);
 	}
 	document.getElementById("card-container").innerHTML = cardArray.join('');
+	addCardListeners();
+}
+
+function addCardListeners() {
+	"use strict";
+	const btnMoreInfo = document.querySelectorAll('.btn-more-info');
+	const CardPoster = document.querySelectorAll('.poster-wrap');
+	const btnFavorite = document.querySelectorAll('.btn-favorite');
+	const elements = [btnMoreInfo, CardPoster, btnFavorite];
+	for (let i = 0; i < elements.length; i++) {
+		forEachElem (elements[i], i);
+	}
+	function forEachElem (element, i) {
+		element.forEach( elem => {
+			const id = elem.dataset.id;
+			if (i === 2) {
+				elem.addEventListener('click', () => {
+					if (JSON.stringify(localStorage).indexOf(id) > 0) {
+						localStorage.removeItem(id);
+						elem.classList.remove('active');
+					} else {
+						localStorage.setItem(id, 'id');
+						elem.classList.add('active');
+					}
+				});
+			} else {
+				elem.addEventListener('click', () => {
+					fetch(`${url}&i=${id}&plot=full`)
+					.then( response => response.json() )
+					.then( data => generateModalMoreInfo(data) );
+				});
+			}
+		});
+	}
+}
+
+function generateModalMoreInfo(data) {
+	"use strict";
+	const arrayKeys = ['Released', 'Genre', 'Country', 'Director', 'Actors', 'Runtime', 'Language', 'Production', 'Website', 'BoxOffice', 'imdbRating', 'imdbVotes'];
+	let modalDescription = '';
+	const localstorege = JSON.stringify(localStorage);
+	let favorite ='';
+	if (localstorege.indexOf(data.imdbID) > 0) {favorite = 'active';}
+	for (let i = 0; i < arrayKeys.length; i++) {
+		if (data[arrayKeys[i]] !== "N/A" && data[arrayKeys[i]] !== undefined) {
+			modalDescription += `<span class="modal-description small"><strong>${arrayKeys[i]}:</strong> ${data[arrayKeys[i]]}</span><br>`;
+		}
+	}
+	const modalContent = `
+	  <div class="modal-header">
+        <h5 class="modal-title" id="ModalTitle">${data.Title}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		  <img src="${data.Poster}" alt="" class="modal-poster">
+		  <div class="modal-about">
+			  <h5 class="modal-subtitle">Plot</h5>
+			  <ul class="list-group list-group-flush">
+				  <li class="list-group-item text-justify">${data.Plot}</li>
+			  </ul>
+			  ${modalDescription} 
+		  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn-modal-favorite btn btn-outline-warning ${favorite}" data-id="${data.imdbID}"><i class="fa fa-star-o"></i> Favorites</button>
+      </div>
+	`;
+	document.getElementsByClassName('modal-content')[0].innerHTML = modalContent;
+	addModalListeners();
+	$('#modalMoreInfo').modal('show');
+}
+
+function addModalListeners() {
+	"use strict";
+	const btnModalFavorite = document.querySelector('.btn-modal-favorite');
+	const id = btnModalFavorite.dataset.id;
+		btnModalFavorite.addEventListener('click', () => {
+			if (JSON.stringify(localStorage).indexOf(id) > 0) {
+				localStorage.removeItem(id);
+				btnModalFavorite.classList.remove('active');
+				document.getElementById(id).classList.remove('active');
+			} else {
+				localStorage.setItem(id, 'id');
+				btnModalFavorite.classList.add('active');
+				document.getElementById(id).classList.add('active');
+			}
+		});
 }
